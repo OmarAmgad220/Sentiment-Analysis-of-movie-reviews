62c046e58828335aa65468c1e4989a4a89b95ea0
Ali Ashraf <alyashrafelprince2004@gmail.com>
2025-04-18 22:29:31 +0200

    Finished Deployment

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ import pickle
 app = Flask(__name__)
 
 # load the feature extractor and models
-# featureExtractor = pickle.load(open('models/featureExtractionVectorizer.pkl', 'rb'))
+LogisticRegression = pickle.load(open('models/Logistic_Regression.pkl', 'rb'))
+
+# load the saved vectorizer
+vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
 
 # load positive and negative arrays
 try:
@@ -16,18 +19,28 @@ except:
     positive = []
     negative = []
 
-
 # home menu
 @app.route('/')
 def home():
-    return render_template('index.html', positive=positive, negative=negative)
+    return render_template('index.html', positive=positive, negative=negative, predict = '')
 
 
 # predict results
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    return render_template('index.html', positive=positive, negative=negative)
+    if request.method == 'POST':
+        input_text = request.form['content']
+        features = vectorizer.transform([input_text])
+        prediction = LogisticRegression.predict(features)[0]
 
+        if prediction == 1:
+            positive.append(input_text)
+            pickle.dump(positive, open('data/positive.pkl', 'wb'))
+            return render_template('index.html', positive=positive, negative=negative, predict='Positive')
+        else:
+            negative.append(input_text)
+            pickle.dump(negative, open('data/negative.pkl', 'wb'))
+            return render_template('index.html', positive=positive, negative=negative, predict='Negative')
 
 if __name__ == "__main__":
     app.run()
