5640c1569c7264fb4c2ba9bde98f274e63c2ccd2
Ali Ashraf <alyashrafelprince2004@gmail.com>
2025-04-29 05:51:06 +0300

    Fixed Model loading bug + updated reviews to display used model in prediction

--- app.py
+++ app.py
@@ -10,17 +10,20 @@ app = Flask(__name__)
 # load the feature extractor
 vectorizer = pickle.load(open('models/tfidf_vectorizer.pkl', 'rb'))
 
-# load positive and negative arrays and the models
-try:
-    positive = pickle.load(open('data/positive.pkl', 'rb'))
-    negative = pickle.load(open('data/negative.pkl', 'rb'))
-    models = {
+# load the models
+
+models = {
         "SVM": pickle.load(open('models/SVM.pkl', 'rb')),
         "LogisticRegression": pickle.load(open('models/Logistic_Regression.pkl', 'rb')),
         "NaiveBayes": pickle.load(open('models/Naive_Bayes.pkl', 'rb')),
         "RandomForest": pickle.load(open('models/Random_Forest.pkl', 'rb')),
         "KNN": pickle.load(open('models/KNN.pkl', 'rb')),
-    }   
+}  
+
+# load positive and negative arrays
+try:
+    positive = pickle.load(open('data/positive.pkl', 'rb'))
+    negative = pickle.load(open('data/negative.pkl', 'rb')) 
 
 except:
     positive = []
@@ -52,19 +55,19 @@ def predict():
     if request.method == 'POST':
         model_name = request.form.get('usedModel', 'SVM')
         input_text = request.form['content']
-        input_text = preprocess_text(input_text)
-        features = vectorizer.transform([input_text])
+        preprocessed_input_text = preprocess_text(input_text)
+        features = vectorizer.transform([preprocessed_input_text])
 
         # Use the selected model from the dictionary
         selected_model = models.get(model_name, models['SVM'])  # Fallback to SVM if model not found
         prediction = selected_model.predict(features)[0]
 
         if prediction == 1:
-            positive.append(input_text)
+            positive.append(input_text + ' - ' + model_name)
             pickle.dump(positive, open('data/positive.pkl', 'wb'))
             return render_template('index.html', positive=positive, negative=negative, predict='Positive', usedModel=model_name)
         else:
-            negative.append(input_text)
+            negative.append(input_text + ' - ' + model_name)
             pickle.dump(negative, open('data/negative.pkl', 'wb'))
             return render_template('index.html', positive=positive, negative=negative, predict='Negative', usedModel=model_name)
 
